65c18fb291b14dd400143f4a9766df5c6e9983ea
K.Mayisela <st10443396@vcconnect.edu.za>
2024-06-10 15:51:23 +0200

    first commit

---
+++ app/src/main/java/com/st10443396/weatherapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.st10443396.weatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private var etAppName: TextView? = null
+    private var etName: TextView? = null
+    private var etStudentNum: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        etAppName = findViewById(R.id.etAppName)
+        etName = findViewById(R.id.etName)
+        etStudentNum = findViewById(R.id.etStudentNum)
+
+        val btnStart = findViewById<Button>(R.id.btnStart)
+        val btnExitAp = findViewById<Button>(R.id.btnExitApp)
+
+    }
+}
